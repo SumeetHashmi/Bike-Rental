50ae37ec044f7f9a4dc4a77e0215f1b2c724fe87
Shazil-55 <shazilnisar7@gmail.com>
2024-06-28 12:58:06 +0500

    Cors issue

--- src/helpers/cors.ts
+++ src/helpers/cors.ts
@@ -1,6 +1,6 @@
 import cors from 'cors';
 
-const allowedOrigins = ['*'];
+const allowedOrigins = ['*' , 'http://localhost:4000/' ];
 
 export const internalOptions: cors.CorsOptions = {
   origin: allowedOrigins,
